26b0d8eea3d257b1ac9a2a1bf1fe16f3e4147c72
Matteo Calisti <matteocalisti@gmail.com>
2025-03-08 18:57:11 +0100

    Basic sidenav

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+function toggleNav() {
+    if (document.getElementById("mainSidenav").style.width === "300px")
+        document.getElementById("mainSidenav").style.width = "0px";
+    else 
+        document.getElementById("mainSidenav").style.width = "300px";
+}
